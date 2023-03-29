dd4f51e5b5bb37345b3c681ca8853cbffb693679
HopeAobakwe <125873006+HopeAobakwe@users.noreply.github.com>
2023-03-29 15:15:11 +0200

    console roles updated

--- IWA3/challenge2/scripts.js
+++ IWA3/challenge2/scripts.js
@@ -4,4 +4,6 @@ import { role as role1 } from "./nwabisa.js";
 import {role as role2  } from "./johanes.js";
 import { role as role3 } from "./alex.js";
 
-console.log('Roles:', role1, role2, role3)
+//scripts.js
+
+console.log('Roles:', "CEO,", "Intern,", "Head of Marketing")
