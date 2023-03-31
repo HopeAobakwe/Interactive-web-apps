c4cd5c1452cd6a94a4ba4e29dc1a4054f798bc24
HopeAobakwe <125873006+HopeAobakwe@users.noreply.github.com>
2023-03-31 13:12:56 +0200

    Update scripts.js

--- IWA5/challenge1/scripts.js
+++ IWA5/challenge1/scripts.js
@@ -4,15 +4,14 @@ NONE_SELECTED = '0'
 
 if (location = RSA) { shipping === 400 && currency === 'R' }
 
-if (location = NAM){
-
-shipping = 600} 
-else {shipping = 800}
+if location = NAM
+shipping = 600
+else shipping = 800
 
 shoes = 300 * 1
 toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries =35 * 2
+hirts = 150 * 'NONE_SELECTED'
+batteries 35 * 2
 pens = 5 * 'NONE_SELECTED' 
 
 shipping = null
