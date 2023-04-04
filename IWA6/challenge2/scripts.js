fc007817b01d8dc1f5974b196af550d7a4b18235
HopeAobakwe <125873006+HopeAobakwe@users.noreply.github.com>
2023-04-04 15:07:36 +0200

    attempt 1

---
+++ IWA6/challenge2/scripts.js
@@ -0,0 +1,15 @@
+const rent = 400;
+const tax = '12%';
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00;
+const minuteOfDay = 00;
+
+// Only change below this line
+if (hourOfDay === 00 && minuteOfDay === 00) {
+  const taxAsDecimal = number(tax.replace(%/'')) / 100;
+  const salaryAfterTax = (salary * 1 )- taxAsDecimal;
+  const balance = salaryAfterTax - transport - food - rent;
+  console.log('R' + balance.toFixed(2));
+}
